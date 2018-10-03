6db54ce6c4c46e2dde57cb3ead254514e9d14f2a
Madhu <madhu@iGamesDevIndias-MacBook-Pro.local>
2018-10-03 16:36:08 +0530

    Committing the latest code.

---
+++ SampleSwiftApp/SampleSwiftApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SampleSwiftApp
+//
+//  Created by Madhu on 03/10/18.
+//  Copyright © 2018 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
